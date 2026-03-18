f27e53531dd954f8fdc9abecd644d9c556adf839
Mahir Jain <mahirjain2006@gmail.com>
2026-03-19 00:02:36 +0530

    update

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -41,7 +41,7 @@ export default function AppRoutes() {
         <Route path="/live/:roomId" element={<ProtectedRoute><LiveInteractionRoom /></ProtectedRoute>} />
         <Route path="/matchmaking" element={<ProtectedRoute><MatchingBuffer /></ProtectedRoute>} />
         <Route path="/profile" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
-        <Route path="/session" element={<ProtectedRoute><PostSessionModal /></ProtectedRoute>} />
+        <Route path="/session/:roomid" element={<ProtectedRoute><PostSessionModal /></ProtectedRoute>} />
         <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
         <Route path="/homepage" element={<ProtectedRoute><HomePageScreen /></ProtectedRoute>} />
         <Route path="/active-users" element={<ProtectedRoute><ActiveUsersScreen /></ProtectedRoute>} />
